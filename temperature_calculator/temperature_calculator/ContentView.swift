f8b5169b2b32b168338e464bbaad584142abc05f
Borja Sáez de Guinoa Vilaplana <borja.saezdeguinoa@gmail.com>
2021-05-11 11:02:34 +0200

    init commit

---
+++ temperature_calculator/temperature_calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  temperature_calculator
+//
+//  Created by Borja Saez de Guinoa Vilaplana on 11/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ temperature_calculator/temperature_calculator/temperature_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  temperature_calculatorApp.swift
+//  temperature_calculator
+//
+//  Created by Borja Saez de Guinoa Vilaplana on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct temperature_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
